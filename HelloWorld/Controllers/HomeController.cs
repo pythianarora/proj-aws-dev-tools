ea5f46324769251d3d816435429d7a3dc9162851
Sandeep Arora <arora@pythian.com>
2018-05-31 22:06:01 +0530

    Hello World Application

---
+++ HelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Hello from AWS CodeCommit, CodeBuild, CodeDeploy and CodePipleline. Congrats on getting this to work.";
+            return View();
+        }
+    }
+}
